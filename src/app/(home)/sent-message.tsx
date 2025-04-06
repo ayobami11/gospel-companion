e6278a47c09daf41a56bd2a7f316863244f6f47f
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2025-04-06 19:11:20 +0100

    Fixed styling issues.

--- src/app/(home)/sent-message.tsx
+++ src/app/(home)/sent-message.tsx
@@ -1,6 +1,6 @@
 const SentMessage = ({ message }: { message: string }) => {
     return (
-        <div className="bg-white rounded-[1.25rem] rounded-ee-none w-fit self-end py-3 px-4 dark:bg-[hsl(0,0%,23%)] dark:text-white">
+        <div className="bg-stone-200 rounded-[1.25rem] rounded-ee-none w-fit self-end py-3 px-4 dark:bg-[hsl(0,0%,23%)] dark:text-white">
             {message}
         </div>
     )
